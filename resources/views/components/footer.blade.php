4e7824fa73e2e20f864e3149412e016bbcab7a20
Shuhrat666 <shuhratodilbekov3@gmail.com>
2025-05-21 20:25:58 +0500

    PATCH: modified for server 2

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-light text-center py-3 mt-5 border-top">
     <div class="container">
-        <small>© {{ date('Y') }} Expense Tracker.Open-source project. All rights reserved.</small>
+        <small>© {{ date('Y') }} Expense Tracker. <a href="https://github.com/SShuhratt/Expense-tracker" target="_blank">Open-source project</a>. All rights reserved.</small>
     </div>
 </footer>
